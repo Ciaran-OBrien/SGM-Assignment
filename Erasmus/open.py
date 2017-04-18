cfb1c19927b12f540b15c52f5ed11086a82c1208
Ciaran-OBrien <ciaran.obrien10@gmail.com>
2017-04-18 17:29:44 +0100

    Merged the two seperate py files into one, and loading the secodn window through a popup window

--- Erasmus/open.py
+++ Erasmus/open.py
@@ -25,8 +25,113 @@ try:
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
+class Ui_Erasmus(QtGui.QWidget):
+    def __init__(self,parent = None):
+        super(Ui_Erasmus, self).__init__(parent)
+        self.setupUi(self)
+    def setupUi(self, Erasmus):
 
-class Ui_Form(object):
+        Erasmus.setObjectName(_fromUtf8("Erasmus"))
+        Erasmus.resize(1458,1000)
+        self.horizontalLayout = QtGui.QHBoxLayout(Erasmus)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.scrollArea = QtGui.QScrollArea(Erasmus)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName(_fromUtf8("scrollArea"))
+        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 623, 782))
+        self.scrollAreaWidgetContents.setObjectName(_fromUtf8("scrollAreaWidgetContents"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+
+        self.gridLayout_2.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.horizontalLayout.addWidget(self.scrollArea)
+        self.scrollArea_2 = QtGui.QScrollArea(Erasmus)
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName(_fromUtf8("scrollArea_2"))
+        self.scrollAreaWidgetContents_2 = QtGui.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 588, 938))
+        self.scrollAreaWidgetContents_2.setObjectName(_fromUtf8("scrollAreaWidgetContents_2"))
+        self.gridLayout_3 = QtGui.QGridLayout(self.scrollAreaWidgetContents_2)
+        self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
+        self.gridLayout = QtGui.QGridLayout()
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+
+        self.gridLayout_3.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.horizontalLayout.addWidget(self.scrollArea_2)
+
+        self.createBtns(Erasmus)
+        self.createTextBrowser(Erasmus)
+        self.loadFile("Austria",Erasmus)
+
+        QtCore.QMetaObject.connectSlotsByName(Erasmus)
+
+    def createBtns(self,Erasmus):
+        self.countries=[] #Create empty list
+        afile=open('countries.txt','r') #Open file for reading
+        for line in afile: #iterate through file and add each item to the list
+             self.countries.append(str(line).rstrip('\n'))
+        afile.close()
+        self.btns = {}
+        i=1;
+        for country in self.countries:
+            self.btns[i] = QtGui.QPushButton(self.scrollAreaWidgetContents)
+            self.btns[i].setObjectName(_fromUtf8(country))
+            self.verticalLayout.addWidget(self.btns[i])
+            self.btns[i].setText(_translate("Erasmus", country, None))
+            self.btns[i].clicked.connect(partial(self.loadFile,self.btns[i].text(),Erasmus))
+            i += 1
+
+    def createTextBrowser(self,Erasmus):
+
+
+        self.textBrowser = {}
+        i=0
+        for row in range(0,5):
+            for col in range(0,2):
+                print("Row: ", row ,"Col: ", col)
+                self.textBrowser[i] = QtGui.QTextBrowser(self.scrollAreaWidgetContents_2)
+                self.textBrowser[i].setObjectName(_fromUtf8("textBrowser"))
+                self.gridLayout.addWidget(self.textBrowser[i], row, col, 1, 1)
+                i +=1
+
+
+    def retranslateUi(self, Erasmus):
+
+        Erasmus.setWindowTitle(_translate("Erasmus", "Erasmus", None))
+        i=0
+        for row in range(0,5):
+            for col in range(0,2):
+                self.textBrowser[i].setHtml(_translate("Erasmus", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                "p, li { white-space: pre-wrap; }\n"
+                "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">" + self.university[i] + "</span></p>\n"
+                "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;\"><br /></p></body></html>", None))
+                i +=1
+    def loadFile(self,btn,Erasmus):
+        print(btn)
+        self.countries=[] #Create empty list
+        afile=open('countries.txt','r') #Open file for reading
+        for line in afile: #iterate through file and add each item to the list
+            self. countries.append(str(line).rstrip('\n'))
+        afile.close()
+
+        self.university=[]
+        afile=open(btn + '/university.txt','r')
+        for line in afile: #iterate through file and add each item to the list
+             self.university.append(str(line).rstrip('\n'))
+        afile.close()
+        self.retranslateUi(Erasmus)
+
+class Ui_Open(QtGui.QWidget):
+    def __init__(self,parent = None):
+        super(Ui_Open, self).__init__(parent)
+        self.setupUi(self)
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
         Form.resize(1458, 1000)
@@ -79,9 +184,15 @@ class Ui_Form(object):
         self.setLocal(Form)
         self.retranslateUi(Form)
         self.comboBox.setCurrentIndex(0)
-        # self.pushButton_2.clicked.connect(partial(self.setLocal,self.pushButton_2.objectName(),Form))
-        # self.pushButton.clicked.connect(partial(self.setLocal,self.pushButton.objectName(),Form))
+        # self.pushButton_2.clicked.connect(partial(self.on_pushButton_clicked))
+        # self.pushButton.clicked.connect(partial(self.on_pushButton_clicked))
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
         QtCore.QMetaObject.connectSlotsByName(Form)
+
+
+    def on_pushButton_clicked(self):
+        dialog = Ui_Erasmus(self)
+        dialog.show()
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Form", None))
@@ -128,11 +239,10 @@ class Ui_Form(object):
         print ("Current Index", self.comboBox.currentIndex())
 
 
+
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
-    Form = QtGui.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
+    main = Ui_Open()
+    main.show()
     sys.exit(app.exec_())
